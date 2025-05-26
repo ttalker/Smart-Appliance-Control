fb8a3f677401285c7e035fd147cbcf34582aa58b
Kelvin Rivera <kelvindaverivera26@gmail.com>
2025-05-26 14:59:26 +0800

    added test window for gui

--- src/mainapp/Main.java
+++ src/mainapp/Main.java
@@ -3,5 +3,9 @@ package mainapp;
 public class Main {
   public static void main(String[] args) {
     System.out.println("Hello World!");
+  
+    MainWindow window = new MainWindow();
+    window.setVisible(true);
+      
   }
 }
--- src/mainapp/mainWindow.java
+++ src/mainapp/mainWindow.java
@@ -1,3 +1,22 @@
 package mainapp;
+import java.awt.*;
+import javax.swing.*;
 
+public class MainWindow extends JFrame {
 
+    public MainWindow() {
+        setTitle("Main Window");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(400, 300);
+        setLocationRelativeTo(null);
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new GridLayout(20, 1, 5, 5));
+        for (int i = 1; i <= 20; i++) {
+            panel.add(new JLabel("Label number " + i));
+        }
+
+        JScrollPane scrollPane = new JScrollPane(panel);
+        getContentPane().add(scrollPane);
+    }
+}
